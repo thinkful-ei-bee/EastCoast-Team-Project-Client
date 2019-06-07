11ea805657c4701d4de867f2d966127e69a80f8c
Farah Student <farah.gus@hotmail.com>
2019-06-07 10:38:28 -0400

    added handlers for registration form

--- src/Components/RegistrationForm/RegistrationForm.js
+++ src/Components/RegistrationForm/RegistrationForm.js
@@ -1,27 +1,66 @@
 import React from 'react'
+import AuthApiService from '../../Services/auth-api-service'
 import './RegistrationForm.css'
 
 export default class RegistrationForm extends React.Component{
+  static defaultProps = {
+    registrationSuccess: () => { }
+  }
+
+  state = { error: null }
+
+  firstInput = React.createRef()
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { name, email, password} = e.target
+    AuthApiService.postUser({
+      name: name.value,
+      email: email.value, 
+      password: password.value
+    })
+    .then(user => {
+      name.value = ''
+      email.value = ''
+      password.value = ''
+      this.props.registrationSuccess()
+    })
+    .catch(res => {
+      this.setState({ error: res.error })
+    })
+  }
+
+  componentDidMount() {
+    this.firstInput.current.focus()
+  }
+
   render(){
+    const { error } = this.state
     return(
       <section>
-        <div className="registration-form">
-        <form >
+        <div className="registration-form" >
+        <fieldset>
+          <form onSubmit={this.handleSubmit}>
+            
+            <div role='alert'>
+              {error && <p>{error}</p>}
+            </div>
+            
+            <label htmlFor="registration">Full Name</label>
+            <input type="text" id="full-name" name="full-name" required/>
 
-          <label htmlFor="registration">Full Name</label>
-          <input type="text" id="full-name" name="full-name" required/>
+            <label htmlFor="email">Email</label>
+            <input type="text" id="email" name="email" required/>
 
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" required/>
+            <label htmlFor="Password">Password</label>
+            <input type="text" id="password" name="password" required/> 
+            
+            <label htmlFor="comfirmPassword">Confirm Password</label>
+            <input type="text" id="comfirm-password" name="confirm-password" required/>
 
-          <label htmlFor="Password">Password</label>
-          <input type="text" id="password" name="password" required/> 
-          
-          <label htmlFor="comfirmPassword">Confirm Password</label>
-          <input type="text" id="comfirm-password" name="confirm-password" required/>
-
-          <button type="submit">Sign up!</button>       
-        </form>
+            <button type="submit">Sign up!</button>       
+          </form>
+        </fieldset>
         </div>
       </section>
     )
--- src/Routes/RegistrationRouter/RegistrationRoute.js
+++ src/Routes/RegistrationRouter/RegistrationRoute.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import RegistrationForm from '../../Components/RegistrationForm/RegistrationForm';
 
-export default function RegistrationRoute() {
-  return(
-    <div>
-      <RegistrationForm />
-    </div>
-  )
+export default class RegistrationRoute extends React.Component {
+  static defaultProps = {
+    history: {
+      push: () => {},
+    }
+  }
+
+  handleRegistrationSuccess = () => {
+    const { history } = this.props
+    history.push('/login')
+  }
+  
+  render() {
+    return(
+    <section>
+      <h1>Rendezvous registration</h1>
+      <div>
+        <RegistrationForm registrationSucess={this.handleRegistrationSuccess} />
+      </div>
+    </section>
+    )
+  }  
 }
