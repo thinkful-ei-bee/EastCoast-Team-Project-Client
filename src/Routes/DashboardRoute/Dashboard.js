9ab2bd5f24f7be3c4fb8701694912a25efbc9da8
Farah Student <farah.gus@hotmail.com>
2019-06-09 18:00:35 -0400

    console log

--- src/Routes/DashboardRoute/Dashboard.js
+++ src/Routes/DashboardRoute/Dashboard.js
@@ -11,6 +11,7 @@ export default class Dashboard extends React.Component{
   componentDidMount() {
     ProfileService.getProfile()
       .then(profile => {
+        console.log.log(profile)
         this.setState({
 
         })
