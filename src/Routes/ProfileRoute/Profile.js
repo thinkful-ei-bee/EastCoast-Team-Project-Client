945d3d8e1bb7395a0c89d8d18c0fa0cc02c5f82a
LaurelButler <laurelbutler126@gmail.com>
2019-06-10 19:19:32 -0400

    working on user profile

--- src/Routes/ProfileRoute/Profile.js
+++ src/Routes/ProfileRoute/Profile.js
@@ -3,22 +3,37 @@ import ProfileService from '../../Services/profile-service'
 
 export default class Profile extends React.Component{
   state = {
-
+    userInfo: []
   }
 
   componentDidMount() {
     ProfileService.getProfile()
       .then(profile => {
-        console.log(profile)
+        // console.log(profile)
         this.setState({
-
+          userInfo: profile
         })
       })
   }
 
   render(){
+    console.log(this.state.userInfo);
+    // const users = userInfo.map((info) => 
+    // <userInfo {...info} />
+    // )
+    const users = this.state.userInfo.map((info) =>
+      <UserInfo {...info} />
+    )
     return(
-      <div>Profile</div>
+      <div>
+        <UserInfo users={users} />
+      </div>
     )
   }
+}
+
+const UserInfo = props => {
+  return (
+    <h1>{UserInfo}</h1>
+  )
 }
