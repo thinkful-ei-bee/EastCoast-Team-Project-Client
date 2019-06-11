7d16efe71dd2eaabf9c35d44e1ea36140467b2c2
Farah Student <farah.gus@hotmail.com>
2019-06-11 18:47:26 -0400

    added events to profile

--- src/Routes/ProfileRoute/ProfileOther.js
+++ src/Routes/ProfileRoute/ProfileOther.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProfileService from '../../Services/profile-service'
+import EventService from '../../Services/events-service'
 import './Profile.css'
 
 export default class ProfileOther extends React.Component{
   state = {
     profile: [],
+    events: []
   }
 
   componentDidMount() {
@@ -12,15 +14,31 @@ export default class ProfileOther extends React.Component{
       .then(profile => {
         this.setState({ profile: profile})
       })
+    
+    EventService.getEvents()
+      .then(event => {
+        const filteredEvents = event.filter(e => e.event_owner_id === this.state.profile.id)
+        this.setState({
+          events: filteredEvents
+        })
+      })
+
   }
 
   render() {
-    console.log(this.state.profile)
     const user = this.state.profile
+    const events = this.state.events
+    const userEvents = (events.length === 0 ) ? 'I have no events yet' : events
     return(
       <div className="profile">
         <img src={user.profile_picture} alt=''/>
         <p>Bio: {user.me_intro}</p>
+        <p>Interests:</p>
+        <ul>
+          <li>Music: {user.music_like}</li>
+          <li>Favorite movie: {user.movie_like}</li>
+        </ul>
+        <p>My events: {userEvents}</p>
       </div>
     )
   }
