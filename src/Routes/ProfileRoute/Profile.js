52116c13aa13af12c35c6de84096df7e405be179
LaurelButler <laurelbutler126@gmail.com>
2019-06-11 12:02:21 -0400

    profile form

--- src/Routes/ProfileRoute/Profile.js
+++ src/Routes/ProfileRoute/Profile.js
@@ -27,6 +27,7 @@ export default class Profile extends React.Component{
     return(
       <div>
         <UserInfo users={users} />
+        <UserProfile />
       </div>
     )
   }
@@ -36,4 +37,40 @@ const UserInfo = props => {
   return (
     <h1>{UserInfo}</h1>
   )
+}
+
+class UserProfile extends React.Component {
+  state = {
+    music_like: '',
+    movie_like: '',
+    me_intro: ''
+  }
+  render () {
+    return (
+      <form>
+        <label htmlFor="me_intro">Bio:</label>
+        <input
+          required
+          name="me_intro"
+          type="text"
+          value={this.state.me_intro}
+        />
+        <label htmlFor="music_like">Music Interests:</label>
+        <input
+          required
+          name="music_like"
+          type="text"
+          value={this.state.music_like}
+        />
+        <label htmlFor="movie_like">Movie Interests:</label>
+        <input
+          required
+          name="movie_like"
+          type="text"
+          value={this.state.movie_like}
+        />
+        <button type="submit">Sumbit</button>
+      </form>
+    )
+  }
 }
