e7d7e95cf0aad2c3e21d26e362dfba726c7e9537
Farah Student <farah.gus@hotmail.com>
2019-06-10 20:30:14 -0400

    css

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -18,9 +18,9 @@ export default class Nav extends React.Component{
         {this.context.user.name}
       </span>
       <nav>
-        <Link onClick={this.handleLogoutClick} to='/login'>Logout</Link>
         <Link to='/'>Dashboard</Link><br></br>
-        <Link to='/notifications'>Notifications</Link>
+        <Link to='/notifications'>Notifications</Link><br></br>
+        <Link onClick={this.handleLogoutClick} to='/login'>Logout</Link>
       </nav>
     </div>
   )
