82ddc3e5db147e96063e52813ebfc0ecec9c4ace
Farah Student <farah.gus@hotmail.com>
2019-06-11 11:20:10 -0400

    config endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://sleepy-journey-32906.herokuapp.com/api',
+    API_ENDPOINT: 'http://localhost:8000/api',
     TOKEN_KEY: 'redezvous-client-auth-token',
   }
   
