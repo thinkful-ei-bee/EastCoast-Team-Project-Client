7276709b72c6820f9ab775854754715b0a8e6d8e
Farah Student <farah.gus@hotmail.com>
2019-06-11 22:47:49 -0400

    filtered

--- src/Routes/DashboardRoute/Dashboard.js
+++ src/Routes/DashboardRoute/Dashboard.js
@@ -12,6 +12,7 @@ export default class Dashboard extends React.Component{
     allProfileInfo: [],
     filteredProfileInfo: [],
     events: [],
+    currentImage: [],
     currentImageIndex: 0,
     showEventifyForm: false,
     selectValue: false
@@ -34,18 +35,20 @@ export default class Dashboard extends React.Component{
           allUsers: profile
         })
         
-        const allUsers = (!this.state.allUsers) ? [] : this.state.allUsers
-        const loggedinUser = this.state.currentUser
+      const allUsers = (!this.state.allUsers) ? [] : this.state.allUsers
+      const loggedinUser = this.state.currentUser
 
-        // get gender of logged in user from id
-        const loggedinUserGender = loggedinUser.map(user => user.gender)
+      // get gender of logged in user from id
+      const loggedinUserGender = loggedinUser.map(user => user.gender)
 
-        // filter users whose gender does not match the logged in user gender
-        const filteredUsers = allUsers.filter(user => user.gender !== loggedinUserGender.toString())
+      // filter users whose gender does not match the logged in user gender
+      const filteredUsers = allUsers.filter(user => user.gender !== loggedinUserGender.toString())
 
-        this.setState({
-          filteredProfileInfo: filteredUsers
-        })
+      this.setState({
+        filteredProfileInfo: filteredUsers
+      })
+      console.log(this.state.filteredProfileInfo[0].id)
+     
     })
   }
 
@@ -53,9 +56,12 @@ export default class Dashboard extends React.Component{
     // find index of last image in the array
     const lastIndex = this.state.filteredProfileInfo.length - 1;
 
+    const { currentImageIndex } = this.state;
+
     //check if we need to start over from the last index
-    const resetIndex = this.state.currentImageIndex === 0;
-    const index = resetIndex ? lastIndex : this.state.currentImageIndex - 1;
+    const resetIndex = currentImageIndex === 0;
+
+    const index = resetIndex ? lastIndex : currentImageIndex - 1;
 
     //assign the logical index to currentImageIndex that will use in render method
     this.setState({
@@ -67,9 +73,11 @@ export default class Dashboard extends React.Component{
     //find index of the last image in array
     const lastIndex = this.state.filteredProfileInfo.length - 1;
 
+    const { currentImageIndex } = this.state;
+
     //check if we need to start over from the last index
-    const resetIndex = this.state.currentImageIndex === lastIndex;
-    const index = resetIndex ? 0 : this.state.currentImageIndex + 1;
+    const resetIndex = currentImageIndex === lastIndex;
+    const index = resetIndex ? 0 : currentImageIndex + 1;
 
     //assign the logical index to currentImageIndex that will use in render method
     this.setState({
@@ -92,27 +100,33 @@ export default class Dashboard extends React.Component{
   }
 
   render(){
-    // get current image index
-    const index = this.state.currentImageIndex;
+    // console.log(Object.keys(this.state.filteredProfileInfo[this.state.currentImageIndex]))
+    console.log(this.state.currentImageIndex)
 
-    // create new array with 1 image with the source images
-    let firstImage = this.state.filteredProfileInfo.slice(index, index + 1);
+    // const currentImage = (!this.state.filteredProfileInfo) ? [] : this.state.filteredProfileInfo[this.state.currentImageIndex].id
+    // console.log(currentImage)
+    // // get current image index
+    // const index = this.state.currentImageIndex;
 
-    //check length of new array 
-    if (firstImage.length < 1) {
-      firstImage = firstImage.concat(this.state.filteredProfileInfo.slice(0, 1-firstImage.length))
-    }
+    // // create new array with 1 image with the source images
+    // let firstImage = this.state.filteredProfileInfo.slice(index, index + 1);
 
-
+    // //check length of new array 
+    // if (firstImage.length < 1) {
+    //   firstImage = firstImage.concat(this.state.filteredProfileInfo.slice(0, 1-firstImage.length))
+    // }
     return(
       <div className="dashboard">
         <div className="dashboard-pic">
           <button className="left-btn btn" onClick={this.prevPicture}>{'<'}</button>
           <div className="picture-carousel">
-          {firstImage.map((pic, index) => 
+          {/* {this.state.currentImage.map((pic, index) => 
             <Link to={`/profile/${pic.id}`} key={index}><img src={pic.profile_picture} alt=''/></Link>
-            )}
+            )} */}
+            {/* {this.state.filteredProfileInfo.map(pic => <img src={pic.profile_picture}/>)} */}
+          <img src={this.state.filteredProfileInfo[0]}/>
           </div>
+
           <button className="right-btn btn" onClick={this.nextPicture}>{'>'}</button>
         </div>
       
