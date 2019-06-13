0091e8f55bc852bfc64d91fe38e0e507bdf22359
LaurelButler <laurelbutler126@gmail.com>
2019-06-13 13:09:06 -0400

    icons are working

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import {BrowserRouter} from 'react-router-dom'
 import {UserProvider} from './contexts/UserContext'
 import './index.css';
 import App from './App';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fab)
 
 ReactDOM.render(
 <BrowserRouter>
