325621bd93adbf8f5e0ce49ab4aeed8bd6d32220
Farah Student <farah.gus@hotmail.com>
2019-06-21 11:44:15 -0400

    fixed date string

--- src/Routes/EventsRoute/EventsPage.js
+++ src/Routes/EventsRoute/EventsPage.js
@@ -18,14 +18,16 @@ export default class EventsPage extends React.Component{
 
   render(){
     const event = this.state.events
+    const date = (!event.event_date) ? '' : event.event_date.slice(0, 10)
+    const time = (!event.event_time) ? '' : event.event_time.slice(0, 5)
+    console.log(time)
 
     return(
       <div className="events-page">
         <h2 className="event-name">{event.event_name}</h2>
-        {/* <div className="event-picture">Event pic</div> */}
         <div>
           <h3>Event info:</h3>
-          <p>When: {event.event_date} at {event.event_time}</p>
+          <p>When: {date} at {time}</p>
           <p>Where: {event.event_location}</p>
           <p>Details: {event.event_details}</p>
         </div>
